89f5da702f1938d591504486194a464f47061faf
ryan <ryan@call-levels.com>
2020-08-16 09:55:56 +0800

    Added Vue project from vue-cli. webpack not configured for prod yet

--- view2/src/App.vue
+++ view2/src/App.vue
@@ -2,13 +2,18 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/dashboard">Dashboard</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- view2/src/views/Home.vue
+++ view2/src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="home">
+<div class="home">
+    <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+        <div class="flex-shrink-0">
+            <img class="h-12 w-12" src="../assets/logo.svg" alt="Logo">
+        </div>
+        <div class="ml-6 pt-1">
+            <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
+            <p class="text-base text-gray-600 leading-normal">You have a new message!</p>
+        </div>
+    </div>
+
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+</div>
 </template>
 
 <script>
